9afb36a56bd518c0821f6c335604006d2bddfb7d
hchac003 <hchac003@fiu.edu>
2017-02-08 21:33:40 -0500

    Adding code for Algorithm 3.1 and some additional refactoring.

--- algos/routing.py
+++ algos/routing.py
@@ -5,6 +5,10 @@ from math import ceil, log2
 from typing import List, Set
 
 
+class NonSquareMatrix(Exception):
+    pass
+
+
 class GraphNotSet(Exception):
     pass
 
@@ -17,6 +21,15 @@ class VertexNonExistent(Exception):
     pass
 
 
+class Vertex(object):
+    __slots__ = ['rep', 'cluster', 'flag']
+
+    def __init__(self, rep, cluster, flag):
+        self.rep = rep
+        self.cluster = cluster
+        self.flag = flag
+
+
 class Routing(object):
     def __init__(self, M: List[List[float]] = None) -> None:
         self.set_graph(M)
@@ -26,39 +39,52 @@ class Routing(object):
         # the graph
         return nx.floyd_warshall_numpy(G).max()
 
-    def set_graph(self, M: List[List[float]]) -> None:
-        if M is None:
-            self._mat = None
-            self._graph = None
-            return
+    def _convert_power_of_2_diameter(self, np_mat):
+        """Convert any adjacency matrix which denotes some graph G = (V, E, w)
+        into an adjacency matrix which denotes some graph G' = (V, E, w_c) with
+        diameter equal to some power of 2.
 
-        adj_mat = np.matrix(M)
+        NOTE:
+            The given adjacency matrix MUST denote a STRONGLY connected graph.
 
-        # ======== Turn graph into graph with diameter 2^h for some h =========
-
-        # Convert all edges with weight 0 to infinity. 0 weight edge delimits
-        # that there does not exist an edge between the two vertices in the
-        # adjacency matrix.
-        adj_mat[adj_mat == 0.0] = np.inf
-        G_min_edge = adj_mat.min()
+        """
+        # Negative weights delimit a non-existent edge between two nodes, which
+        # is equivalent to edge weight of infinity.
+        np_mat[np_mat <= 0.0] = np.inf  # @TODO: Should weight 0.0 be allowed?
+        G_min_edge = np_mat.min()
 
         epsilon = np.float(0.01)
         # @TODO: check to see if there's a faster way of doing this
         vec_func = np.vectorize(lambda x: ((1+epsilon) / G_min_edge) * x)
-        adj_mat = vec_func(adj_mat)
+        np_mat = vec_func(np_mat)
 
-        G_p = nx.MultiDiGraph(adj_mat)
+        G_p = nx.MultiDiGraph(np_mat)
         G_p_diam = self._graph_diameter(G_p)
 
         # @TODO: check to see if there's a faster way of doing this
         vec_func = np.vectorize(
             lambda x: ((2 ** ceil(log2(G_p_diam))) / G_p_diam) * x
         )
-        adj_mat = vec_func(adj_mat)
-        # =====================================================================
+        np_mat = vec_func(np_mat)
 
-        self._mat = adj_mat
-        self._graph = nx.MultiDiGraph(adj_mat)
+        return np_mat
+
+    def set_graph(self, M: List[List[float]]) -> None:
+        if M is None:
+            self._mat = None
+            self._n_vertices = None
+            self._graph = None
+            self._diam = None
+            return
+
+        adj_mat = np.matrix(M)
+
+        if adj_mat.shape[0] != adj_mat.shape[1]:
+            raise NonSquareMatrix
+
+        self._mat = self._convert_power_of_2_diameter(adj_mat)
+        self._n_vertices = self._mat.shape[0]
+        self._graph = nx.MultiDiGraph(self._mat)
         self._diam = self._graph_diameter(self._graph)
 
     def r_neighborhood(self, v, r: int) -> Set[int]:
@@ -73,6 +99,55 @@ class Routing(object):
             raise VertexNonExistent
 
         return set(nbhd.keys())
+
+    def _randomized_HDS_gen(self):
+        V = frozenset(np.arange(self._n_vertices))
+
+        # @TODO: check that this is a uniform permutation
+        pi = np.random.permutation(self._n_vertices)
+        U = np.random.uniform(.5, 1)
+        h = int(log2(self._diam))
+        H = [None] * (h + 1)
+
+        H[h] = set()
+        H[h].add(V)
+
+        working_dict = {}
+        for v in V:
+            working_dict[v] = Vertex(None, None, True)
+
+        for i in reversed(range(0, h)):
+            H[i] = set()
+
+            for C in H[i+1]:
+                for v in C:
+                    ver = working_dict[v]
+                    ver.cluster = set()
+                    ver.flag = True
+
+                    ver.rep = None
+                    for j in pi:
+                        if j in (C & (self.r_neighborhood(v, U * 2**(i-1)))):
+                            ver.rep = j
+                            break
+
+                    # Something is wrong if this triggers
+                    assert(ver.rep is not None)
+
+                for v in C:
+                    v_ver = working_dict[v]
+                    for u in C:
+                        u_ver = working_dict[u]
+                        if u_ver.flag and u_ver.rep == v:
+                            u_ver.flag = False
+                            v_ver.cluster.add(u)
+
+                for v in C:
+                    ver = working_dict[v]
+                    if v_ver.cluster:
+                        H[i].add(frozenset(ver.cluster))
+
+        return H
 
     def get_path(self, algo, s, t: int) -> List[int]:
         if self._graph is None:
