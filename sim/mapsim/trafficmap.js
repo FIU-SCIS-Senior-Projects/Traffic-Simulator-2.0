9b4454136219860ea234695b0f4df43ec78579f1
max leone - macbook <maxleone@maxs-MacBook-Pro.local>
2017-03-07 01:08:54 -0500

    Start integrate mapquest api

--- sim/mapsim/trafficmap.js
+++ sim/mapsim/trafficmap.js
@@ -1,17 +1,38 @@
+// Properties/Settings (in future could come from a file or db)
+var centerLat = 25.786;
+var centerLon = -80.257;
+var boundsDeltaLon = 0.2;
+var boundsDeltaLat = 0.1;
+var maxZ = 14;
+var minZ = 12;
+var startZ = 13;
+
+// Set Map Properties
+var center = L.latLng(centerLat, centerLon);
+var northEastBound = L.latLng(centerLat - boundsDeltaLat, centerLon + boundsDeltaLon);
+var southWestBound = L.latLng(centerLat + boundsDeltaLat, centerLon - boundsDeltaLon);
+
+// Map Layer for MapQuest Plugin
+var mapLayer = MQ.mapLayer()
+
 // Init Map
 var map = L.map('map', {
-    maxZoom: 17,
-    minZoom: 13,
-    maxBounds: [
-        //south west
-        [25.73, -80.4],
-        //north east
-        [25.77, -80.37]
-        ], 
-}).setView([25.752, -80.38], 15);
+	layers: mapLayer,
+    maxZoom: maxZ,
+    minZoom: minZ,
+    maxBounds: [southWestBound, northEastBound], 
+}).setView(center, startZ);
 
-// Add base Tiles
-L.tileLayer('https://api.mapbox.com/styles/v1/mleon111/cizys0tdi005s2ssjrr49y7a9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWxlb24xMTEiLCJhIjoiY2l6eXJuc2ZpMDA4NzJxa2Y3b3BiZjlpNSJ9.yzZAypPBZkyEHk28nNkG2w', 
-{
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+// Add Traffic data from Map Quest Plugin
+L.control.layers({
+  'Map': mapLayer,
+  'Satellite': MQ.satelliteLayer(),
+  'Hybrid': MQ.hybridLayer(),
+  'Dark': MQ.darkLayer(),
+  'Light': MQ.lightLayer()
+}, {
+  'Traffic Flow': MQ.trafficLayer({layers: ['flow']}),
+  'Traffic Incidents': MQ.trafficLayer({layers: ['incidents']})
 }).addTo(map);
+
+
