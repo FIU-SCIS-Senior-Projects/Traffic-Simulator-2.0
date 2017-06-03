0465dd11027e29d27c10f49e782a55931f427102
Matt Thomson <red.cataclysm@gmail.com>
2017-06-03 18:35:53 -0400

    Completes geojson adj matrix

--- Code/server/routes/graph.js
+++ Code/server/routes/graph.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const spawn = require('child_process').spawn;
+const PythonShell = require('python-shell');
 
 const geojson = require('../data/big_geo');
 
 router.post('/', (req, res) => {
-  res.json({ msg: 'Post to /graph' });
+  // let py_initGraph = spawn('python', ['./server/algo/graph/node_wrapper.py']);
+  // let dataString = '';
+
+  // py_initGraph.stdout.on('data', (data) => {
+  //   dataString += data.toString();
+  // });
+
+  // py_initGraph.stdout.on('end', () => {
+  //   console.log(`dataString: ${dataString}`);
+  //   res.json({ msg: `POST /graph ${dataString}` });
+  // });
+
+  // // console.log('adjMatrix', JSON.stringify(req.body.adjMatrix));
+  // console.log('stuff')
+  // // py_initGraph.stdin.write(JSON.stringify(req.body.adjMatrix));
+  // console.log(JSON.stringify(req.body.adjMatrix).length);
+  // py_initGraph.stdin.write(JSON.stringify(req.body.adjMatrix));
+  // py_initGraph.stdin.end();
+
+
+  let pyshell = new PythonShell('./server/algo/graph/node_wrapper.py');
+
+  pyshell.send(JSON.stringify(req.body.adjMatrix));
+
+  pyshell.on('message', (msg) => {
+    console.log(msg);
+  });
+
+  pyshell.end((err) => {
+    if (err) {
+      console.log('error', err);
+      res.status(500).send('error');
+    }
+
+    console.log('finished');
+    res.json({ msg: `POST /graph` });
+  });
 });
 
 router.get('/geo', (req, res) => {
--- Code/server/static/js/test.js
+++ Code/server/static/js/test.js
@@ -3919,15 +3919,16 @@ function testPathPost () {
 
   test.then((res) => {
     console.log(res);
-    results.append('p { POST /graph success }');
+    results.append('p { POST /path success }');
   }).catch((err) => {
-    results.append('p { POST /graph failure }');
+    results.append('p { POST /path failure }');
   });
 }
 
-function testGraphPost () {
+function testGraphPost (data) {
   let test = new Promise((resolve, reject) => {
     __WEBPACK_IMPORTED_MODULE_0_superagent___default.a.post('http://localhost:8080/graph')
+      .send(data)
       .end((err, res) => {
         if (err) {
           reject(err);
@@ -3940,10 +3941,10 @@ function testGraphPost () {
   });
 
   test.then((res) => {
-    console.log(res);
-    results.append('p { POST /path success }');
+    console.log('POST /graph', res);
+    results.append('p { POST /graph success }');
   }).catch((err) => {
-    results.append('p { POST /path failure }');
+    results.append('p { POST /graph failure }');
   });
 }
 
@@ -3977,6 +3978,7 @@ function testGeoJSON () {
     let edges = [];
     let startNode;
     let endNode;
+    console.log(`# Features: ${res.features.length}`);
     res.features.forEach((feature) => {
       let coords = [];
       startNode = null;
@@ -3987,10 +3989,10 @@ function testGeoJSON () {
 
       // console.log(`Nodes length ${nodes.length}`);
       for (var i = 0; i < nodes.length; i++) {
-        if (arrayEqual(nodes[i].latlng, coords[0])) {
+        if (ArraysEqual(nodes[i].latlng, coords[0])) {
           startNode = nodes[i];
         }
-        if (arrayEqual(nodes[i].latlng, coords[coords.length - 1])) {
+        if (ArraysEqual(nodes[i].latlng, coords[coords.length - 1])) {
           endNode = nodes[i];
         }
       }
@@ -4013,11 +4015,15 @@ function testGeoJSON () {
         linePoints: coords,
         polyLine: null
       };
+      // console.log('coords', coords);
+      // let rev = arrayRev(coords);
+      // console.log('rev', rev);
 
       let reverseEdge = {
         startNode: endNode,
         endNode: startNode,
         linePoints: coords.reverse(),
+        // linePoints: rev,
         polyLine: null
       };
 
@@ -4029,19 +4035,31 @@ function testGeoJSON () {
     });
 
     let adjMatrix = initAdjacenyMatrix(nodes, edges);
-    console.log('Adjacency Matrix', adjMatrix);
+    // console.log('Adjacency Matrix', adjMatrix);
+    let reduced = adjMatrix.reduce((acc, v) => {
+      return acc + v.reduce((acc, e) => {
+        return acc + e;
+      }, 0)
+    }, 0)
+    console.log('1st init', reduced);
+
+    TestAdjacencyMatrixForSingleConnectedNodes(adjMatrix, nodes, edges);
+
+    adjMatrix = initAdjacenyMatrix(nodes, edges);
+    reduced = adjMatrix.reduce((acc, v) => {
+      return acc + v.reduce((acc, e) => {
+        return acc + e;
+      }, 0)
+    }, 0)
+    console.log('2nd init', reduced);
     // test matrix for empty rows
     // test for deadend rows
     // test for single connected nodes
-
-    __WEBPACK_IMPORTED_MODULE_0_superagent___default.a.post('http://localhost:8080/test')
-      .set('Content-Type', 'application/json')
-      .send({ adjMatrix: adjMatrix })
-      .end((err, res) => {
-        console.log(res);
-      });
+    testGraphPost({adjMatrix, adjMatrix});
+  
 
   }).catch((err) => {
+    console.log('err', err);
     results.append('p { POST /graph/geo failure }');
   });
 
@@ -4072,28 +4090,174 @@ function initAdjacenyMatrix (nodes, edges) {
   return adjacencyMatrix;
 }
 
-function EuclideanDistance(a, b)
-{
-  return Math.sqrt(Math.pow((b[0] - a[0]), 2) + Math.pow((b[1] - a[1]), 2));
-}
 
-function arrayEqual (arr1, arr2) {
-  if (arr1 instanceof Array &&
-      arr2 instanceof Array &&
-      arr1.length === arr2.length &&
-      arr1.every(function (e, i) {
-        return e === arr2[i];
-      }))
+function TestAdjacencyMatrixForSingleConnectedNodes(adjacencyMatrix, nodes, edges)
+{
+  console.log("Testing adjacency matrix for single connected nodes...");
+  var singleConnectedRows = [];
+  for(var i = 0; i < adjacencyMatrix.length; i++)
   {
-        return true;
+    var zeroCount = 0;
+    for(var j = 0; j < adjacencyMatrix.length; j++)
+    {
+      if(adjacencyMatrix[i][j] == 0)
+      {
+        zeroCount++;
+      }
+    }
+    if(zeroCount == adjacencyMatrix.length-1)
+    {
+      singleConnectedRows.push(i);
+
+      var closestNode = FindClosestNode(nodes[i], nodes);
+      var edge = {startNode: nodes[i], endNode: closestNode, linePoints: [nodes[i].latlng, closestNode.latlng], polyLine: null};
+      var reverseEdge = {startNode: closestNode, endNode: nodes[i], linePoints: [closestNode.latlng, nodes[i].latlng], polyLine: null};
+      edges.push(edge);
+      edges.push(reverseEdge);
+      
+    }
+  }
+  if(singleConnectedRows.length > 0)
+  {
+    console.log("Single connected nodes found in adjacency matrix at rows...");
+    console.log(singleConnectedRows);
+    console.log("Edge List has been modified, updating adjacency matrix...");
+    // Since this test can modify the edge list the adjacency matrix needs to be updated
+  }
+  else
+  {
+    console.log("No single connected nodes found in adjacency matrix");
   }
 
-  return false;
+
 }
+
+function FindClosestNode(node, nodes)
+{
+  //console.log("Finding closest node to node: " + node.index);
+  var closest;
+  if(node.index < nodes.length-2)
+  {
+    closest = nodes[node.index + 1];
+  }
+  else
+  {
+    closest = nodes[node.index - 1];
+  }
+  
+  var lastDistance = EuclideanDistance(node.latlng, closest.latlng);
+
+  for(var i = node.index; i < nodes.length; i++)
+  {
+    if(i != node.index)
+    {
+      var distance = EuclideanDistance(node.latlng, nodes[i].latlng)
+      if(distance < lastDistance)
+      {
+        closest = nodes[i];
+        lastDistance = distance;
+      }
+    }
+  }
+
+  for(var i = nodes.length-1; i > 0; i--)
+  {
+    if(i != node.index)
+    {
+      var distance = EuclideanDistance(node.latlng, nodes[i].latlng)
+      if(distance < lastDistance)
+      {
+        closest = nodes[i];
+        lastDistance = distance;
+      }
+    }
+  }
+
+  //console.log(closest);
+  return closest;
+}
+
+
+
+
+
+
+
+// Helper to get a random float in a range
+// function RandomFloatRange(min, max) 
+// {
+//   return Math.random() < 0.5 ? ((1-Math.random()) * (max-min) + min) : (Math.random() * (max-min) + min);
+// }
+
+// function RandomIntRange(min, max) 
+// {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// Helper to get Euclidean Distance between two latlng coords
+function EuclideanDistance(coordsA, coordsB)
+{
+  return Math.sqrt(Math.pow((coordsB[0] - coordsA[0]), 2) + Math.pow((coordsB[1] - coordsA[1]), 2));
+}
+
+// function Normalize(val, max, min) 
+// { 
+//   return (val - min) / (max - min); 
+// }
+
+
+// Helper to check if two arrays are equal
+function ArraysEqual(arr1, arr2) 
+{
+    if(arr1.length !== arr2.length) {
+        return false;
+      }
+
+    for(var i = arr1.length; i >= 0; i--) {
+        if(arr1[i] !== arr2[i]) {
+            return false;
+          }
+    }
+
+    return true;
+}
+
+// helper function to store the reverse of an array without mutating the original and returning the reversed array
+// because in javascript Array.reverse() mutates the original before the console can print so its hard to debug
+function arrayRev(arr)
+{
+  var reverseArr = [];
+
+  for(var i = arr.length-1; i >= 0; i--)
+  {
+    reverseArr.push(arr[i]);
+  }
+
+  return reverseArr;
+}
+
+// use: console.log([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(normalize(5, 15)));
+// function ArrayNormalize(min, max) {
+//     var delta = max - min;
+//     return function (val) {
+//         return (val - min) / delta;
+//     };
+// }
+
+
+
+
+
+
+
+
+
 
 function init () {
   testPathPost();
-  testGraphPost();
+  // testGraphPost();
   testGeoJSON();
 }
 
