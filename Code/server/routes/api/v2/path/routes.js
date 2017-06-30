046578b76a3d0c9021d7b4050aa7a761c2a2c7bc
Matt Thomson <red.cataclysm@gmail.com>
2017-06-30 17:28:56 -0400

    Moved vertices server side

--- Code/server/routes/api/v2/path/routes.js
+++ Code/server/routes/api/v2/path/routes.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const pathAPI = require('./index');
+const vertices = require('../../../../data/vertices.js');
 
 router.post('/dijkstra', (req, res) => {
   // Later use graph from session, but for now init a new one. 
   pathAPI.dijkstra(req.session, req.body.source, req.body.destination)
   // pathAPI.dijkstra()
     .then((path) => {
+      path = path.map((point) => {
+        return { lng: vertices.features[point].geometry.coordinates[0], lat: vertices.features[point].geometry.coordinates[1] };
+      });
       return res.json({ path: path });
     })
     .catch((err) => {
